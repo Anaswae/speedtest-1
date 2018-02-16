157f503cc4b5058614d8e3946cd6cdbe2c607e14
Muhammad Rifky Abimayu <34646726+rifkytech@users.noreply.github.com>
2018-02-16 14:19:37 +0700

    Bug Fix

--- speedtest_worker.js
+++ speedtest_worker.js
@@ -1,7 +1,7 @@
 /*
-	HTML5 Speedtest v4.5.2
-	by Federico Dossena
-	https://github.com/adolfintel/speedtest/
+	Mexious Internet Speedtest v4.5.2
+	by Mexious Media
+  https://github.com/rifkytech/speedtest/
 	GNU LGPLv3 License
 */
 
@@ -31,7 +31,7 @@ var settings = {
   url_dl: 'garbage.php', // path to a large file or garbage.php, used for download test. must be relative to this js file
   url_ul: 'empty.php', // path to an empty file, used for upload test. must be relative to this js file
   url_ping: 'empty.php', // path to an empty file, used for ping test. must be relative to this js file
-  url_getIp: 'getIP.php', // path to getIP.php relative to this js file, or a similar thing that outputs the client's ip
+  url_getIp: 'https://speedtest.mexio.us/getIP.php', // path to getIP.php relative to this js file, or a similar thing that outputs the client's ip
   getIp_ispInfo: true, //if set to true, the server will include ISP info with the IP address
   xhr_dlMultistream: 10, // number of download streams to use (can be different if enable_quirks is active)
   xhr_ulMultistream: 3, // number of upload streams to use (can be different if enable_quirks is active)
